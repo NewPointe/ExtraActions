483418f3a158e0a32b8f1fc7b0e4591eeef7332f
Tyler Schrock <tschrock123@gmail.com>
2017-06-08 14:01:00 -0400

    New Actions

---
+++ org.newpointe.ExtraActions/ObjectConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace org.newpointe.WorkflowEntities
+{
+    public class ObjectConverter
+    {
+        public static object ConvertObject(String theObject, Type objectType, bool tryToNull = true)
+        {
+            if (objectType.IsEnum)
+            {
+                return String.IsNullOrWhiteSpace(theObject) ? null : Enum.Parse(objectType, theObject, true);
+            }
+
+            Type underType = Nullable.GetUnderlyingType(objectType);
+            if (underType == null) // not nullable
+            {
+                return Convert.ChangeType(theObject, objectType);
+            }
+
+            if (tryToNull && String.IsNullOrWhiteSpace(theObject))
+            {
+                return null;
+            }
+            return Convert.ChangeType(theObject, underType);
+        }
+    }
+}
--- org.newpointe.ExtraActions/Properties/AssemblyInfo.cs
+++ org.newpointe.ExtraActions/Properties/AssemblyInfo.cs
@@ -1,35 +1,35 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("{{{OrgPrefix}}}.{{{ProjectName}}}")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Extra Actions")]
+[assembly: AssemblyDescription("Extra Workflow Actions for Rock RMS")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("{{{OrgName}}}")]
-[assembly: AssemblyProduct("{{{OrgPrefix}}}.{{{ProjectName}}}")]
-[assembly: AssemblyCopyright("Copyright © {{{CurrentYear}}}")]
+[assembly: AssemblyCompany("NewPointe Community Church")]
+[assembly: AssemblyProduct("ExtraActions")]
+[assembly: AssemblyCopyright("Copyright ©  2017 NewPointe Community Church")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("{{{ProjectGuid}}}")]
+[assembly: Guid("89d22c1d-7a5d-4255-a2b5-3165fe85f168")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
